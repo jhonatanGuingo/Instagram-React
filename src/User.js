a3bab7c0dc06914ad62db7b69c0fe948bf55c528
jhonatanGuingo <jhonatan.amaral@engenharia.ufjf.br>
2023-05-08 14:49:39 -0300

    funcao alterar img e nome usuario

--- src/User.js
+++ src/User.js
@@ -1,12 +1,36 @@
+import React from "react"
 
 export default function User(){
+    const [userName, SetUsername] = React.useState("catanacomics");
+    const [imgUser, SetimgUser] = React.useState("assets/img/catanacomics.svg");
+
+    function perguntarNome(){
+        const nome = prompt('Qual seu nome de usuario?')
+        if (!nome){
+            return;
+        }
+        SetUsername(nome);
+    }
+
+    function alterarImg (){
+        const img = prompt('Coloque um endereço de imagem:')
+        if (!img){
+            return;
+        }
+        SetimgUser(img);
+    }
+
     return(
         <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+          <img data-test="profile-image" src={imgUser} onClick = {alterarImg} alt="imagem de perfil"/>
           <div class="texto">
-            <span>
-              <strong>catanacomics</strong>
-              <ion-icon name="pencil"></ion-icon>
+            <span data-test="name"> 
+              <strong>{userName}</strong>
+              <ion-icon 
+              data-test="edit-name" 
+              name="pencil"
+              onClick = {perguntarNome}
+              ></ion-icon>
             </span>
           </div>
         </div>
